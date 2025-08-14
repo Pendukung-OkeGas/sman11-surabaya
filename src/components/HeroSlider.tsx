357998f573a9fa3cde70c58608e45fe8b67bb118
Pendukung-OkeGas <izunindah@gmail.com>
2025-08-14 18:51:06 +0700

    edit selengkapnya

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -60,7 +60,7 @@ const HeroSlider = () => {
         <div
           key={slide.id}
           className={`absolute inset-0 transition-opacity duration-1000 ${
-            index === currentSlide ? "opacity-100" : "opacity-0"
+            index === currentSlide ? "opacity-100" : "opacity-0 pointer-events-none"
           }`}
         >
           <div
